aba251b6191fdb3f55521560606482c9f2a027fb
Subrata Das <sxsubra.das@gmail.com>
2024-06-09 12:11:54 +0530

    sensor file success

--- parallel_dags/xcom_spending_natwest.py
+++ parallel_dags/xcom_spending_natwest.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from airflow.sensors.base_sensor_operator import BaseSensorOperator
+from airflow.sensors.base import BaseSensorOperator
 from airflow.utils.decorators import apply_defaults
 from datetime import datetime
 import timeit
@@ -15,10 +15,11 @@ class MyCustomSensor(BaseSensorOperator):
         self.task_to_pull = task_to_pull
     
     def poke(self, context):
+        print(f"Poking at time interval : {timeit.default_timer()}")
         flag = False
         xcom_value = context["ti"].xcom_pull(key=self.xcom_to_pull, task_ids=self.task_to_pull)
         if xcom_value is not None:
-            print(f"Time of sensor True for {self.xcom_to_pull} : {timeit.default_timer}")
+            print(f"Time of sensor True for {self.xcom_to_pull} : {timeit.default_timer()}")
             flag = True
         return flag
 
@@ -70,7 +71,8 @@ weekly_sensor = MyCustomSensor(
     dag = dag,
     xcom_to_pull = "weekly_aggs",
     task_to_pull = "aggregates_task",
-    mode = "poke"
+    mode = "poke",
+    poke_interval = 1
 )
 
 weekly_insights = PythonOperator(
@@ -87,7 +89,8 @@ monthly_sensor = MyCustomSensor(
     dag = dag,
     xcom_to_pull = "monthly_aggs",
     task_to_pull = "aggregates_task",
-    mode = "poke"
+    mode = "poke",
+    poke_interval = 1
 )
 
 monthly_insights = PythonOperator(
